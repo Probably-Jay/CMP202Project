dcd0f85769d99f5d346c1da5c29875a43c5def08
JACOB BRETHERTON <jake.bretherton05@gmail.com>
2020-02-23 19:40:28 +0000

    refacro into cpp

--- CMP202Project/TimingData.cpp
+++ CMP202Project/TimingData.cpp
@@ -1,1 +1,56 @@
 #include "TimingData.h"
+
+TimingData::TimingData(string name, void(*f)(), int ittr, bool consoleOut)
+	:name(name), function(f), iterations(ittr), consoleOut(consoleOut) {
+	auto timeStart = system_clock::to_time_t(system_clock::now()); // current time for file name
+	filename = "Timings\\" + name + to_string(timeStart) + ".csv";
+
+	file.open(filename);
+}
+
+TimingData::~TimingData()
+{
+	if (file.is_open())
+		file.close();
+}
+
+void TimingData::Close()
+{
+	if (file.is_open())
+		file.close();
+}
+
+void TimingData::RunTiming()
+{
+	tryOpen();
+	for (int i = 0; i < iterations; i++) {
+		beginTime = high_resolution_clock::now();
+		callFunc(function); // inline function call to do the timed function
+		endTime = high_resolution_clock::now();
+		elapsedTime = duration_cast<duration<double>>(endTime - beginTime);
+		timings.push_back(elapsedTime.count());
+		string output = to_string(elapsedTime.count()) + ',';
+		file << output;
+	}
+}
+
+double TimingData::CalculateMedianTime()
+{
+
+	auto timingList = timings;
+
+	sort(timingList.begin(), timingList.end()); // sort the list so we can find the middle value
+
+	medianTiming = timingList[timingList.size() / (int)2]; // get the middle time
+
+	record(to_string(medianTiming)); // record the time
+
+	return medianTiming;
+
+}
+
+void TimingData::record(string data)
+{
+	tryOpen();
+	file << '\n' + data;
+}
--- CMP202Project/TimingData.h
+++ CMP202Project/TimingData.h
@@ -19,58 +19,21 @@ using namespace std::chrono;
 class TimingData
 {
 public:
-	TimingData(string name, void (*f)(), int ittr, bool consoleOut) :name(name), function(f), iterations(ittr), consoleOut(consoleOut) {
-		auto timeStart = system_clock::to_time_t(system_clock::now()); // current time for file name
-		filename = "Timings\\" + name + to_string(timeStart) + ".csv";
-		
-		file.open(filename);
-		
-	}
-	~TimingData() {
-		if (file.is_open())
-			file.close();
-	}
+	TimingData(string name, void (*f)(), int ittr, bool consoleOut);
 
-	void Close() {
-		if (file.is_open())
-			file.close();
-	}
+	~TimingData();
 
-	void RunTiming() {
-		tryOpen();
-		for (int i = 0; i < iterations; i++) {
-			beginTime = high_resolution_clock::now();
-			callFunc(function); // inline function call to do the timed function
-			endTime = high_resolution_clock::now();
-			elapsedTime = duration_cast<duration<double>>(endTime - beginTime);
-			timings.push_back(elapsedTime.count());
-			string output = to_string(elapsedTime.count()) + ',';
-			file << output;
-		}
-	}
+	void Close();
 
-	double CalculateMedianTime() {
+	void RunTiming();
 
-		auto timingList = timings;
-
-		sort(timingList.begin(), timingList.end()); // sort the list so we can find the middle value
-
-		medianTiming = timingList[timingList.size() / (int)2]; // get the middle time
-
-		record(to_string(medianTiming)); // record the time
-
-		return medianTiming;
-
-	}
+	double CalculateMedianTime();
 	
-	void record(string data) {
-		tryOpen();
-		file << '\n' + data;
-	}
-	inline void tryOpen() {
-		if (!file.is_open()) { file.open(filename);};
-	}
+	void record(string data);
+
+	inline void tryOpen() {if (!file.is_open()) { file.open(filename);};}
 	inline void callFunc(void(*f)()) { (*f)(); };
+
 	string name;
 	vector<double> timings;
 	float medianTiming;
