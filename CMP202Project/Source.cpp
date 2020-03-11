5f47d6b68cc49a4fb5a3d8bf1d6dfa647c304717
JACOB BRETHERTON <jake.bretherton05@gmail.com>
2020-03-11 17:11:21 +0000

    not "Working"

--- CMP202Project/Source.cpp
+++ CMP202Project/Source.cpp
@@ -2,9 +2,15 @@
 #include <iostream>
 #include <thread>
 
-using thread_local;
 using std::cout;
 using std::endl;
+
+const int WIDTH = 1920;
+const int HEIGHT = 1200;
+
+// The image data.
+// Each pixel is represented as 0xRRGGBB.
+uint32_t image[HEIGHT][WIDTH];
 
 void test() {
 	//cout << "hello" << endl;
@@ -14,14 +20,62 @@ void test() {
 }
 
 
-int main() {
-	FunctionTimer timer;
-	timer.NewTiming("test", test, 10000, true);
-	
 
-	parallel_for_each(
+void write_tga(const char* filename)
+{
+	ofstream outfile(filename, ofstream::binary);
+
+	uint8_t header[18] = {
+		0, // no image ID
+		0, // no colour map
+		2, // uncompressed 24-bit image
+		0, 0, 0, 0, 0, // empty colour map specification
+		0, 0, // X origin
+		0, 0, // Y origin
+		WIDTH & 0xFF, (WIDTH >> 8) & 0xFF, // width
+		HEIGHT & 0xFF, (HEIGHT >> 8) & 0xFF, // height
+		24, // bits per pixel
+		0, // image descriptor
+	};
+	outfile.write((const char*)header, 18);
+
+	for (int y = 0; y < HEIGHT; ++y)
+	{
+		for (int x = 0; x < WIDTH; ++x)
+		{
+			uint8_t pixel[3] = {
+				image[y][x] & 0xFF, // blue channel
+				(image[y][x] >> 8) & 0xFF, // green channel
+				(image[y][x] >> 16) & 0xFF, // red channel
+			};
+			outfile.write((const char*)pixel, 3);
+		}
+	}
+
+	outfile.close();
+	if (!outfile)
+	{
+		// An error has occurred at some point since we opened the file.
+		cout << "Error writing to " << filename << endl;
+		exit(1);
+	}
+}
+
+int main() {
+	//FunctionTimer timer;
+	//timer.NewTiming("test", test, 10000, true);
+
+
+	for (int y = 0; y < WIDTH / 2.f; y++) {
+		for (int x = 0; x < HEIGHT / 2.f; x++) {
+			image[y][x] = 0xFFFFFFFF;
+		}
+	}
+	write_tga("output.tga");
+	
 
 
 }
+
 
 
